327e76ff892b2239bf11d6f37825e60da369e4eb
bot_kill_site <tantantarou99@gmail.com>
2025-01-09 23:39:49 +0900

    main.js の更新

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-console.log('Hello World!');
+const textarea = document.getElementById('textarea');
+
+textarea.addEventListener('focus', () => {
+  textarea.placeholder = 'コメントを追加…';
+  })
+
+textarea.addEventListener('onkeyup',() => {textarea ='';})
+
+
+  function hatena(event) {
+    let textarea = document.getElementById('textarea');
+    textarea.value = textarea.value.slice(0, -1) + 'おいうんちするんじゃねえクソがぁッッッ！！　あーーーーーマジで反社多すぎてダリいなぁーーーー';
+  }
+
+  
+  
+  function copyText() {
+  let sourceText = document.getElementById('textarea').value;
+  document.getElementById('destination').value = sourceText;
+}
+
+
+
+
+
+
+
+
+
+const tip2 = document.getElementsByClassName('tip2');
+
+tip2.addEventListener('click', () => {textarea = ''; })
+
+
+
+
+
+
+
+
+
+textarea.addEventListener('onkeyup',() => {textarea ='';})
+
+
+
+
+
+
+
+
+
+
+
+
